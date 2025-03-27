248772fe35b51bec1623f036225003722551ca7e
antares <1716607668@qq.com>
2025-03-27 17:15:18 +0800

    完成2道新题

---
+++ src/test/java/leetcode/questions/T4000/T3300/easy/GreedyT.java
@@ -0,0 +1,19 @@
+package leetcode.questions.T4000.T3300.easy;
+
+public class GreedyT {
+    /*
+     * 3216. 交换后字典序最小的字符串
+     */
+    public String getSmallestString(String s) {
+        char[] arr = s.toCharArray();
+        for(int i = 0;i < arr.length-1;i++){
+            if(arr[i]%2 == arr[i+1]%2 && arr[i] > arr[i+1]) {
+                char tmp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = tmp;
+                break;
+            }
+        }
+        return new String(arr);
+    }
+}
---
+++ src/test/java/leetcode/questions/T4000/T3300/medium/DFS.java
@@ -0,0 +1,36 @@
+package leetcode.questions.T4000.T3300.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFS {
+    /*
+     * 3211. 生成不含相邻零的二进制字符串
+     */
+    List<String> ans = new ArrayList<>();
+    public List<String> validStrings(int n) {
+        char[] cur = new char[n];
+        cur[0] = '0';
+        backtrack(cur, 1, n);
+        cur[0] = '1';
+        backtrack(cur, 1, n);
+        return ans;
+    }
+
+    public void backtrack(char[] cur, int index, int n) {
+        if(index == n) {
+            ans.add(new String(cur));
+            return;
+        }
+
+        if(cur[index-1] == '0') {
+            cur[index] = '1';
+            backtrack(cur, index+1, n);
+        } else {
+            cur[index] = '0';
+            backtrack(cur, index+1, n);
+            cur[index] = '1';
+            backtrack(cur, index+1, n);
+        }
+    }
+}
