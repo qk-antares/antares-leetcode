71a0ad9a19b9a637d434155e0eee907cdc44a15e
antares <1716607668@qq.com>
2025-09-01 12:56:10 +0800

    完成今日1题

--- src/test/java/leetcode/datastruture/heap/BasicT.java
+++ src/test/java/leetcode/datastruture/heap/BasicT.java
@@ -75,6 +75,79 @@ public class BasicT {
     }
 
     /*
+     * 1792. 最大平均通过率 [Medium]
+     * 
+     * 用堆，按照班级大小将index添加到堆中，小的在堆顶（或者直接排序）
+     * 逐个取出堆顶元素，看当前班级是否满了，以及是否有多余的student
+     * 统计通过率之和
+     * 这里的问题在于没有考虑到添加了学生后，班级本身的学生总数也会增加
+     * 
+     * 将每个班级看作一个节点，记作(a,b)，则添加一个学生，该节点增加的通过率gain为：
+     * (a+1)/(b+1)-a/b=[(a+1)b-a(b+1)]/b(b+1)=(b-a)/b(b+1)
+     * 首先将所有的节点添加到一个堆中（按gain的大根堆），然后不断地从堆中取出节点，修改节点并重新放回到堆中，直至extraStudents减为0
+     * 最后逐个取出堆中元素，返回答案
+     * 
+     * 此处的class Node可以用record简化，record自带全参构造函数
+     */
+    class Node {
+        int a;
+        int b;
+        double gain;
+
+        public Node(int a, int b, double gain) {
+            this.a = a;
+            this.b = b;
+            this.gain = gain;
+        }
+    }
+
+    public double maxAverageRatio0(int[][] classes, int extraStudents) {
+        Arrays.sort(classes, (o1, o2) -> o1[1] - o2[1]);
+        double sum = 0;
+        int i = 0, n = classes.length;
+        while (extraStudents > 0 && i < n) {
+            int delta = Math.min(extraStudents, classes[i][1] - classes[i][0]);
+            sum += (double) (classes[i][0] + delta) / (classes[i][1] + delta);
+            extraStudents -= delta;
+            i++;
+        }
+
+        while (i < n) {
+            sum += (double) classes[i][0] / classes[i][1];
+            i++;
+        }
+
+        return sum / n;
+
+    }
+
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        PriorityQueue<Node> q = new PriorityQueue<>((n1, n2) -> Double.compare(n2.gain, n1.gain));
+
+        for (int[] c : classes) {
+            int a = c[0];
+            int b = c[1];
+            q.add(new Node(a, b, 1.0 * (b - a) / (1L * b * (b + 1))));
+        }
+
+        while (extraStudents > 0) {
+            Node n = q.poll();
+            int a = n.a + 1;
+            int b = n.b + 1;
+            q.add(new Node(a, b, 1.0 * (b - a) / (1L * b * (b + 1))));
+            extraStudents--;
+        }
+
+        double sum = 0;
+        while (!q.isEmpty()) {
+            Node n = q.poll();
+            sum += 1.0 * n.a / n.b;
+        }
+
+        return sum / classes.length;
+    }
+
+    /*
      * ========================== 分割线 ==========================
      */
 
