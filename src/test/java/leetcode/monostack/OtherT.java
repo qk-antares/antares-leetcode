99b4cc0eb7da3cb08515a57d8bf570eca6edc667
antares <1716607668@qq.com>
2025-11-10 09:32:28 +0800

    完成今日1题

--- src/test/java/leetcode/monostack/OtherT.java
+++ src/test/java/leetcode/monostack/OtherT.java
@@ -2,6 +2,8 @@ package leetcode.monostack;
 
 import java.util.ArrayDeque;
 
+import org.junit.jupiter.api.Test;
+
 /*
  * 单调栈的非常规题
  */
@@ -29,5 +31,36 @@ public class OtherT {
         }
 
         return ans;
+    }
+
+    /*
+     * 3542. 将所有元素变为 0 的最少操作次数 [Medium]
+     * 
+     * 维护一个单调递增栈，栈底到栈顶依次为严格递增的元素
+     * 1) 当遇到的元素大于栈顶元素时，入栈
+     * 2) 当遇到的元素小于栈顶元素时，这表明对于栈顶，其左右一定存在更小的元素，则栈顶要操作1次：出栈,ans++
+     * 3) 当遇到的元素等于栈顶元素时，该元素可以和栈顶一并操作，直接跳过
+     * 
+     * 栈可以直接在原nums上模拟
+     */
+    public int minOperations(int[] nums) {
+        int heapIdx = -1;
+        int ans = 0;
+        for (int i = 0; i < nums.length;) {
+            if (heapIdx < 0 || nums[i] > nums[heapIdx]) {
+                nums[++heapIdx] = nums[i++];
+            } else if (nums[i] < nums[heapIdx]) {
+                heapIdx--;
+                ans++;
+            } else {
+                i++;
+            }
+        }
+        return ans + heapIdx + (nums[0] > 0 ? 1 : 0);
+    }
+
+    @Test
+    void test() {
+        minOperations(new int[] { 1, 2, 1, 2, 1, 2 });
     }
 }
