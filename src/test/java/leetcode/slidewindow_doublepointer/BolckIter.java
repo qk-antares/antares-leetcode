28900c8dbbf4fe7a902ef7fd08c37e9e1af664c5
antares <1716607668@qq.com>
2026-02-04 10:06:53 +0800

    完成今日1题

--- src/test/java/leetcode/slidewindow_doublepointer/BolckIter.java
+++ src/test/java/leetcode/slidewindow_doublepointer/BolckIter.java
@@ -149,7 +149,7 @@ public class BolckIter {
      */
 
     /**
-     * 3637. 三段式数组 I   [Easy]
+     * 3637. 三段式数组 I [Easy]
      */
     public boolean isTrionic(int[] nums) {
         int idx1 = 0;
@@ -172,6 +172,97 @@ public class BolckIter {
             return false;
         // 必须到结尾了
         return idx3 == nums.length - 1;
+    }
+
+    /**
+     * 3640. 三段式数组 II [Hard]
+     * 
+     * 遍历所有这样的三段式数组
+     * 第一段：只保留最后两个数及其前面的正数
+     * 第二段：必须完整保留
+     * 第三段：从第二段的后面开始保留并更新ans
+     * 
+     * 用dp[i][j]表示以nums[i]作为第j段的最后一个数时的子数组最大和
+     * 
+     * 当nums[i]作为第3段的最后一个数时，有两种情况：
+     * 1. nums[i-1]作为第3段的最后一个数，即dp[i-1][3]
+     * 2. nums[i-1]作为第2段的最后一个数，即dp[i-1][2]
+     * 综上可得状态转移方程：
+     * dp[i][3] = max(dp[i-1][3], dp[i-1][2]) + nums[i] (nums[i]>nums[i-1])
+     * 
+     * 同理可得：
+     * dp[i][2] = max(dp[i-1][2], dp[i-1][1])+ nums[i]  (nums[i]<nums[i-1])
+     * dp[i][1] = max(dp[i-1][1], nums[i-1]) + nums[i]  (nums[i]>nums[i-1])
+     * 
+     * dp[i]仅与dp[i-1]有关，可以进行状态压缩，压缩后相当于仅保留了3个变量
+     */
+    public long maxSumTrionic(int[] nums) {
+        long NEG_INF = Long.MIN_VALUE / 2;
+        long ans = NEG_INF;
+        long f1 = NEG_INF, f2 = NEG_INF, f3 = NEG_INF;
+        for(int i = 1; i < nums.length; i++) {
+            int x = nums[i-1];
+            int y = nums[i];
+            f3 = y > x ? Math.max(f3, f2)+y : NEG_INF;
+            f2 = y < x ? Math.max(f2, f1)+y : NEG_INF;
+            f1 = y > x ? Math.max(f1, x)+y : NEG_INF;
+            ans = Math.max(ans, f3);
+        }
+
+        return ans;
+    }
+
+    public long maxSumTrionic0(int[] nums) {
+        int n = nums.length;
+        int idx1 = 0; // 第一段的起点
+        long ans = Long.MIN_VALUE;
+        while (idx1 < n) {
+            long sum = 0;
+
+            int idx2 = idx1; // 第一段的终点/第二段的起点
+            while (idx2 + 1 < n && nums[idx2 + 1] > nums[idx2]) {
+                idx2++;
+            }
+            if (idx2 - idx1 + 1 < 2) {
+                idx1 = idx1 + 1;
+                continue;
+            }
+
+            int idx3 = idx2; // 第二段的终点/第三段的起点，第二段必须完整包含
+            while (idx3 + 1 < n && nums[idx3 + 1] < nums[idx3]) {
+                idx3++;
+                sum += nums[idx3];
+            }
+            if (idx3 - idx2 + 1 < 2) {
+                idx1 = idx2 + 1;
+                continue;
+            }
+
+            int idx4 = idx3; // 第三段的终点
+            while (idx4 + 1 < n && nums[idx4 + 1] > nums[idx4]) {
+                idx4++;
+            }
+            if (idx4 - idx3 + 1 < 2) {
+                idx1 = idx3 + 1;
+                continue;
+            }
+
+            // 加上第一段（从第一段的倒数第3个数开始）
+            sum += nums[idx2] + nums[idx2 - 1];
+            for (int j = idx2 - 2; j >= idx1 && nums[j] > 0; j--) {
+                sum += nums[j];
+            }
+
+            // 加上第三段（从第三段的正数第2个数开始）
+            for (int j = idx3 + 1; j <= idx4; j++) {
+                sum += nums[j];
+                ans = Math.max(ans, sum);
+            }
+
+            idx1 = idx3;
+        }
+
+        return ans;
     }
 
     @Test
